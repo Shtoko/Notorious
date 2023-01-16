705902964b9544ddbdda1f78a135a054d2e5eea3
Shutoko <110824387+Shutoko@users.noreply.github.com>
2023-01-16 18:17:57 -0500

    Updated Main

---
+++ src/main/java/notorious/notorious/Notorious.java
@@ -0,0 +1,17 @@
+package notorious.notorious;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Notorious extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
