42c8868210a0eaa3b85e8f3d50f0beff0e679d0b
Shutoko <110824387+Shutoko@users.noreply.github.com>
2023-01-16 18:54:58 -0500

    Updated Main

--- src/main/java/notorious/notorious/Notorious.java
+++ src/main/java/notorious/notorious/Notorious.java
@@ -1,17 +1,39 @@
 package notorious.notorious;
 
+import notorious.notorious.utils.ColorUtils;
+import notorious.notorious.utils.ConfigDUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class Notorious extends JavaPlugin {
 
+    public static ConfigDUtils Config;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        // config setup
+        Config = new ConfigDUtils(this, "", "config.yml");
+        Config.configOnEnable();
+
+        // plugin starting message
+        Bukkit.getConsoleSender().sendMessage(ColorUtils.changeColorCode
+                (Objects.requireNonNull(Config.getConfig().getString("StartUp-Message"))));
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+        // plugin stopping message
+        Bukkit.getConsoleSender().sendMessage(ColorUtils.changeColorCode
+                (Objects.requireNonNull(Config.getConfig().getString("StartDown-Message"))));
+
+        // saves config
+        ConfigDUtils.save(Config);
+        Bukkit.getConsoleSender().sendMessage(ColorUtils.changeColorCode("&c[Notorious] Saving configs."));
+
     }
 }
---
+++ src/main/java/notorious/notorious/utils/ConfigDUtils.java
@@ -0,0 +1,77 @@
+package notorious.notorious.utils;
+
+import notorious.notorious.Notorious;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ConfigDUtils implements Listener {
+
+    private File file;
+    private File filepath;
+    private final JavaPlugin plugin;
+    private final String name;
+    private final String path;
+    private FileConfiguration config;
+
+    public void saveDefaultConfig() {
+        if (!filepath.exists()) {
+            Bukkit.getConsoleSender().sendMessage("There was a problem creating the config..");
+        }
+        if (!file.exists())
+            this.plugin.saveResource(path + name, false);
+    }
+
+    public void reloadConfig() {
+        if (filepath == null)
+            filepath = new File(plugin.getDataFolder(), path);
+        if (file == null)
+            file = new File(filepath, name);
+        InputStream stream = plugin.getResource(name);
+        if (stream != null) {
+            YamlConfiguration YamlFile = YamlConfiguration.loadConfiguration(new InputStreamReader(stream));
+            config.setDefaults(YamlFile);
+        }
+    }
+
+    public FileConfiguration getConfig() {
+        if (!filepath.exists())
+            this.saveDefaultConfig();
+        if (config == null)
+            this.reloadConfig();
+        return config;
+    }
+
+    public ConfigDUtils(JavaPlugin plugin, String pathname, String filename) {
+        this.plugin = plugin;
+        this.path = pathname;
+        this.name = filename;
+        this.filepath = new File(plugin.getDataFolder(), path);
+        this.file = new File(filepath, name);
+    }
+
+    public void saveConfig() {
+        try {
+            config.save(file);
+        } catch (Throwable t) {
+            Bukkit.getConsoleSender().sendMessage("There was a problem saving the config..");
+        }
+    }
+
+    public static void save(ConfigDUtils config) {
+        config.saveConfig();
+        config.reloadConfig();
+    }
+
+    public void configOnEnable() {
+        Notorious.Config.saveDefaultConfig();
+        Notorious.Config.reloadConfig();
+        Notorious.Config.saveConfig();
+    }
+}
